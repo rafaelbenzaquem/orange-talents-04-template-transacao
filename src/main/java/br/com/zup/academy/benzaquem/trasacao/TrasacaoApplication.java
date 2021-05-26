e80c2d0c71bcc8e8407144a6812c5dc975fa6961
Rafael Benzaquem Neto <rafael.neto@outlook.com>
2021-05-26 19:36:25 -0300

    Setup do projeto

---
+++ src/main/java/br/com/zup/academy/benzaquem/trasacao/TrasacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.benzaquem.trasacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/academy/benzaquem/trasacao/TrasacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.benzaquem.trasacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
